864ca78b42a14626ccbae56e44bf6666a9a25329
yassa4029-hue <yassa4029@gmail.com>
2026-09-25 14:45:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
 
+const languageBtn = document.getElementById("languageBtn");
+
+
+function setLanguage(lang) {
+
+  // تغيير لغة واتجاه الصفحة
+  document.documentElement.lang = lang;
+
+  document.documentElement.dir =
+    lang === "ar" ? "rtl" : "ltr";
+
+
+  // تغيير كل النصوص
+  document
+    .querySelectorAll("[data-ar][data-en]")
+    .forEach((element) => {
+
+      element.textContent =
+        element.dataset[lang];
+
+    });
+
+
+  // تغيير نص زر اللغة
+  languageBtn.textContent =
+    lang === "ar"
+      ? "English 🇬🇧"
+      : "العربية 🇪🇬";
+
+
+  // تغيير عنوان الصفحة
+  document.title =
+    lang === "ar"
+      ? "ياسا عزيز | Portfolio"
+      : "Yassa Aziz | Portfolio";
+
+
+  // حفظ اللغة المختارة
+  localStorage.setItem(
+    "portfolioLanguage",
+    lang
+  );
+}
+
+
+// عند الضغط على زر اللغة
+languageBtn.addEventListener("click", () => {
+
+  const currentLanguage =
+    document.documentElement.lang;
+
+  const newLanguage =
+    currentLanguage === "ar"
+      ? "en"
+      : "ar";
+
+  setLanguage(newLanguage);
+
+});
+
+
+// استرجاع اللغة السابقة
+const savedLanguage =
+  localStorage.getItem("portfolioLanguage") || "ar";
+
+setLanguage(savedLanguage);
